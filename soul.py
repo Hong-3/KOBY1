9f29b8149fdb1ad326d5f5fa2bd6ff8ff20531a7
Hong-3 <billibutcher98@gmail.com>
2026-04-10 23:59:02 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '710898551-XoLEabcya-NoQPHMPshc'
+TOKEN = '8754406928:AAGxfym_P2JXSIOVE2lQHKH8eI-kVKvKekQ'
 MONGO_URI = 'mongodb+srv://Soul:JYAuvlizhw7wqLOb@soul.tsga4.mongodb.net'
-FORWARD_CHANNEL_ID = -100
-CHANNEL_ID = -100
-error_channel_id = -100
+FORWARD_CHANNEL_ID = -1002825754154
+CHANNEL_ID = -1002825754154
+error_channel_id = -1002825754154
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
